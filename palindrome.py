0ddd410e117aaba21ca2271de4444b69fa5e7670
Anas Talha <100064459+talha07-02@users.noreply.github.com>
2023-01-02 15:25:53 +0530

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -1,10 +1,17 @@
-def palindrome(n):
-    print("Orginal string : " , n)
-    print('Reversed string or Palindrome :' , n[::-1])
-    if n == n[::-1]:
-        print(n ,'is a palindrome')
-    else:
-        print(n ,'is not a palindrome')
+def main():
 
-c = input('Enter the string :')
-palindrome(c)
+
+    def palindrome(n):
+        print("Original string : " , n)
+        print('Reversed string or Palindrome :' , n[::-1])
+        if n == n[::-1]:
+            print(n ,'is a palindrome')
+        else:
+            print(n ,'is not a palindrome')
+
+    c = input('Enter the string :')
+    palindrome(c)
+
+    
+if __name__ == "__main__":
+    main()
