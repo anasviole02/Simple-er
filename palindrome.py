d3670a68067f132c3d8ca3fb2aeb5863d5c21a94
Anas Talha <talhaac07@gmail.com>
2023-01-02 14:58:33 +0530

    Initial commit

---
+++ palindrome.py
@@ -0,0 +1,10 @@
+def palindrome(n):
+    print("Orginal string : " , n)
+    print('Reversed string or Palindrome :' , n[::-1])
+    if n == n[::-1]:
+        print(n ,'is a palindrome')
+    else:
+        print(n ,'is not a palindrome')
+
+c = input('Enter the string :')
+palindrome(c)
